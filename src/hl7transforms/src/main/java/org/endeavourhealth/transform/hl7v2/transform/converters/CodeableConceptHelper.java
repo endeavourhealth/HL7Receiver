08faa7322ee248fe49e607dd798543837206e54f
Stuart Dunning <stuart@bearclawsolutions.co.uk>
2017-02-28 08:00:15 +0000

    Tidy

--- src/hl7transforms/src/main/java/org/endeavourhealth/transform/hl7v2/transform/converters/CodeableConceptHelper.java
+++ src/hl7transforms/src/main/java/org/endeavourhealth/transform/hl7v2/transform/converters/CodeableConceptHelper.java
@@ -3,7 +3,6 @@ package org.endeavourhealth.transform.hl7v2.transform.converters;
 import org.endeavourhealth.transform.hl7v2.transform.TransformException;
 import org.hl7.fhir.instance.model.*;
 
-
 public class CodeableConceptHelper {
 
     public static CodeableConcept getCodeableConceptFromString(String code) throws TransformException {
