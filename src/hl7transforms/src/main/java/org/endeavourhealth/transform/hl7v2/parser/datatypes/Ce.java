ff091794feb5df6744a5eadfd52525fbebf30579
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-03-07 18:29:05 +0000

    Add CE component parsers

--- src/hl7transforms/src/main/java/org/endeavourhealth/transform/hl7v2/parser/datatypes/Ce.java
+++ src/hl7transforms/src/main/java/org/endeavourhealth/transform/hl7v2/parser/datatypes/Ce.java
@@ -7,4 +7,11 @@ public class Ce extends Datatype {
     public Ce(GenericDatatype datatype) {
         super(datatype);
     }
+
+    public String getIdentifier() { return this.getComponentAsString(1); }
+    public String getText() { return this.getComponentAsString(2); }
+    public String getCodingSystem() { return this.getComponentAsString(3); }
+    public String getAlternateIdentifier() { return this.getComponentAsString(4); }
+    public String getAlternateText() { return this.getComponentAsString(5); }
+    public String getAlternativeCodingSystem() { return this.getComponentAsString(6); }
 }
