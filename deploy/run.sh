5968ad7b4fd9303285c0371381389f4e7f961bcb
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-04-02 00:45:33 +0100

    Add max heap memory allocation

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,4 +5,4 @@ export CONFIG_JDBC_USERNAME=endeavour
 export CONFIG_JDBC_PASSWORD=
 export CONFIG_JDBC_URL=jdbc:postgresql://127.0.0.1/config
 
-java -jar /opt/hl7receiver/hl7receiver-1.0-SNAPSHOT-jar-with-dependencies.jar -Xmx4g
+java -jar -Xmx4g /opt/hl7receiver/hl7receiver-1.0-SNAPSHOT-jar-with-dependencies.jar
