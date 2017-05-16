cc7b90d9704003b4690e3962a3f5c68ec038345f
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-05-16 22:59:46 +0100

    Add more indexes

--- src/hl7receiver-database/src/main/schema/V1.4-schema.sql
+++ src/hl7receiver-database/src/main/schema/V1.4-schema.sql
@@ -4,3 +4,7 @@
 
 create index log_message_nextattemptdate_ix on log.message (next_attempt_date) 
 where next_attempt_date is not null;
+
+create index log_message_pid1 on log.message (pid1);
+
+create index log_message_pid2 on log.message (pid2);
