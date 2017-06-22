b8b4ca3a500166146bc0f5b012344c12052d31d6
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-06-22 19:16:13 +0100

    Barts constants

--- src/hl7transforms/src/main/java/org/endeavourhealth/hl7transform/transforms/barts/constants/BartsConstants.java
+++ src/hl7transforms/src/main/java/org/endeavourhealth/hl7transform/transforms/barts/constants/BartsConstants.java
@@ -1,19 +1,12 @@
 package org.endeavourhealth.hl7transform.transforms.barts.constants;
 
 public class BartsConstants {
+
     public static final String odsCode = "R1H";
 
-    public static final String sendingFacility = "2.16.840.1.113883.3.2540";  //MSH.4
+    public static final String sendingFacility = "2.16.840.1.113883.3.2540";                                // MSH.4
 
-    public static final String primaryPatientIdentifierAssigningAuthority = "2.16.840.1.113883.3.2540.1";  // PID.3
-    public static final String primaryEpisodeIdentifierTypeCode = "VISITID";  // PV1.19
-    public static final String primaryPractitionerIdentifierTypeCode = "XXXDoNotUse";
-
-//    public static final String locationFacility = servicingFacility;   // PV1.3.4
-//    public static final String locationBuilding = "HOMERTON UH";       // PV1.3.7
-//
-//    public static final String primaryPractitionerIdentifierTypeCode = "Personnel Primary Identifier";
-//
-//    public static final int bartsXpdPrimaryCarePd1FieldNumber = 4;
-
+    public static final String primaryPatientIdentifierAssigningAuthority = "2.16.840.1.113883.3.2540.1";   // PID.3
+    public static final String primaryEpisodeIdentifierTypeCode = "VISITID";                                // PV1.19
+    public static final String primaryPractitionerIdentifierTypeCode = "XXXDoNotUse";                       // XCN.9
 }
