8b4bcc7c0c9078e1708857cf45932402a3de3fd7
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-07-03 22:08:58 +0100

    Fix mapper

--- src/hl7transforms/src/main/java/org/endeavourhealth/hl7transform/mapper/resource/ResourceMapParameters.java
+++ src/hl7transforms/src/main/java/org/endeavourhealth/hl7transform/mapper/resource/ResourceMapParameters.java
@@ -10,13 +10,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ResourceMapParameters {
-    private static final String equalsValueSeperator = "=";
+    private static final String equalsSeperator = "=";
     private static final String keyValuePairSeperator = "-";
     private static final String repeatingValueSeperator = "~";
 
-    private static final String equalsSeperatorEscape = "/$e/";
-    private static final String keyValuePairSeperatorEscape = "/$h/";
-    private static final String repeatingValueSeperatorEscape = "/$t/";
+    private static final String equalsSeperatorEscape = "/$E/";
+    private static final String keyValuePairSeperatorEscape = "/$H/";
+    private static final String repeatingValueSeperatorEscape = "/$T/";
 
     private Map<String, String> parameters = new LinkedHashMap<>();
 
@@ -33,7 +33,7 @@ public class ResourceMapParameters {
         String[] keyValuePairs = StringUtils.split(identifierString, keyValuePairSeperator);
 
         for (String keyValuePair : keyValuePairs) {
-            String[] keyValue = StringUtils.split(keyValuePair, equalsSeperatorEscape);
+            String[] keyValue = StringUtils.split(keyValuePair, equalsSeperator);
 
             if (keyValue.length != 2)
                 throw new MapperException("Could not parse key value pair in resource map identifier string " + identifierString);
@@ -41,7 +41,7 @@ public class ResourceMapParameters {
             String key = keyValue[0];
             String value = keyValue[1];
 
-            resourceMapParameters.parameters.put(key, value);
+            resourceMapParameters.putUnescaped(key, value);
         }
 
         return resourceMapParameters;
@@ -57,15 +57,19 @@ public class ResourceMapParameters {
     }
 
     public ResourceMapParameters put(String key, List<String> multiValues) {
-        put(escapeString(key), StringUtils.join(multiValues
+        putUnescaped(escapeString(key), StringUtils.join(multiValues
                 .stream()
                 .map(t -> escapeString(t))
                 .collect(Collectors.toList()), repeatingValueSeperator));
         return this;
     }
 
+    protected void putUnescaped(String key, String value) {
+        parameters.put(key, value);
+    }
+
     public ResourceMapParameters put(String key, String value) {
-        parameters.put(escapeString(key), escapeString(value));
+        putUnescaped(escapeString(key), escapeString(value));
         return this;
     }
 
@@ -73,19 +77,19 @@ public class ResourceMapParameters {
         return StringUtils.join(parameters
                 .keySet()
                 .stream()
-                .map(t -> t + equalsValueSeperator + StringUtils.deleteWhitespace(StringUtils.defaultString(parameters.get(t))).toUpperCase())
+                .map(t -> t + equalsSeperator + StringUtils.deleteWhitespace(StringUtils.defaultString(parameters.get(t))).toUpperCase())
                 .collect(Collectors.toList()), keyValuePairSeperator);
     }
 
     private String escapeString(String string) {
-        string = StringUtils.replace(string, equalsValueSeperator, equalsSeperatorEscape);
+        string = StringUtils.replace(string, equalsSeperator, equalsSeperatorEscape);
         string = StringUtils.replace(string, keyValuePairSeperator, keyValuePairSeperatorEscape);
         string = StringUtils.replace(string, repeatingValueSeperator, repeatingValueSeperatorEscape);
         return string;
     }
 
     private String unescapeString(String string) {
-        string = StringUtils.replace(string, equalsSeperatorEscape, equalsValueSeperator);
+        string = StringUtils.replace(string, equalsSeperatorEscape, equalsSeperator);
         string = StringUtils.replace(string, keyValuePairSeperatorEscape, keyValuePairSeperator);
         string = StringUtils.replace(string, repeatingValueSeperatorEscape, repeatingValueSeperator);
         return string;
