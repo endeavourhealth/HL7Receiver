6514a68066050321745880f0f3bd6ab49ff1d903
eaiportal <pholm@eaiportal.com>
2018-01-25 09:30:55 +0000

    Added test scenarios to resourceMerge utility

--- src/utility-mergedb-updater/src/main/java/org/endeavourhealth/utilitymergedb/Main.java
+++ src/utility-mergedb-updater/src/main/java/org/endeavourhealth/utilitymergedb/Main.java
@@ -466,13 +466,18 @@ public class Main {
                 LOG.info("MsgCountA44Saved=" + msgCountA44Saved);
                 //******************************************************************************
                 // TEST
+                //******************************************************************************
+                //UUID fakeUUID = UUID.fromString("3ce7aace-5682-43f4-9036-999999999999");
+                //UUID newUUID = dalResourceMerge.resolveMergeUUID(globalserviceId, "Patient", fakeUUID);
+                //LOG.info("Test1 reply=" + newUUID + " result=" + (newUUID.toString().compareTo(fakeUUID.toString()) == 0));
+                //
                 //UUID newUUID = dalResourceMerge.resolveMergeUUID(globalserviceId, "Patient", UUID.fromString("8de0319c-d8a8-450c-b5aa-91c117f8d9e3"));
-                //LOG.info("Test1 reply=" + newUUID + " result=" + (newUUID.toString().compareTo("3ce7aace-5682-43f4-9036-5e4c9f1d8803") == 0));
+                //LOG.info("Test2 reply=" + newUUID + " result=" + (newUUID.toString().compareTo("3ce7aace-5682-43f4-9036-5e4c9f1d8803") == 0));
+                //
                 //Create two-step test
-                //UUID fakeUUID = UUID.fromString("3ce7aace-5682-43f4-9036-9999999999");
                 //dalResourceMerge.insertMergeRecord(globalserviceId, "Patient", UUID.fromString("3ce7aace-5682-43f4-9036-5e4c9f1d8803"), fakeUUID);
                 //newUUID = dalResourceMerge.resolveMergeUUID(globalserviceId, "Patient", UUID.fromString("8de0319c-d8a8-450c-b5aa-91c117f8d9e3"));
-                //LOG.info("Test2 reply=" + newUUID + " result=" + (newUUID.toString().compareTo(fakeUUID.toString()) == 0));
+                //LOG.info("Test3 reply=" + newUUID + " result=" + (newUUID.toString().compareTo(fakeUUID.toString()) == 0));
                 // Remove fakeUUID manually
                 //******************************************************************************
                 resultSet.close();
