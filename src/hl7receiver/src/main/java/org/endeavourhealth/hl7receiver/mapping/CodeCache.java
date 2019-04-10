7ae3836f7e5452c6ed25e7ca587935f0ac5f86f6
Drew Littler <drewlittler@hotmail.com>
2019-04-10 09:33:50 +0100

    Added expiry time to code map cache

--- src/hl7receiver/src/main/java/org/endeavourhealth/hl7receiver/mapping/CodeCache.java
+++ src/hl7receiver/src/main/java/org/endeavourhealth/hl7receiver/mapping/CodeCache.java
@@ -1,17 +1,22 @@
 package org.endeavourhealth.hl7receiver.mapping;
 
 import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.time.DateUtils;
 import org.endeavourhealth.hl7transform.mapper.code.MappedCode;
 import org.endeavourhealth.hl7transform.mapper.code.MappedCodeAction;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CodeCache {
 
+    static int CACHE_TIMEOUT_SECONDS = 60 * 5;
+
     private ConcurrentHashMap<CodeCacheKey, MappedCode> hashMap = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<CodeCacheKey, Date> hashMapExpiryDates = new ConcurrentHashMap<>();
     private List<MappedCodeAction> codeActionsToCache = new ArrayList<>();
 
     public CodeCache(MappedCodeAction... codeActionsToCache) {
@@ -26,7 +31,19 @@ public class CodeCache {
 
         CodeCacheKey codeCacheKey = new CodeCacheKey(context, code, codeSystem, term);
 
-        return hashMap.getOrDefault(codeCacheKey, null);
+        MappedCode ret = hashMap.get(codeCacheKey);
+        Date cacheTime = hashMapExpiryDates.get(codeCacheKey);
+        if (isExpired(cacheTime)) {
+            return null;
+        } else {
+            return ret;
+        }
+    }
+
+    private static boolean isExpired(Date cacheTime) {
+        long msNow = new Date().getTime();
+        long msExpiry = DateUtils.addSeconds(cacheTime, CACHE_TIMEOUT_SECONDS).getTime();
+        return msNow > msExpiry;
     }
 
     public void putMappedCode(String context, String code, String codeSystem, String term, MappedCode mappedCode) {
@@ -42,5 +59,6 @@ public class CodeCache {
         }
 
         hashMap.put(codeCacheKey, mappedCode);
+        hashMapExpiryDates.put(codeCacheKey, new Date());
     }
 }
